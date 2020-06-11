49d75b0d2b63f945225a99de7bcfb368881bdd4a
anime-is-real <aweldon2701@gmail.com>
2020-06-10 22:47:55 -0500

    Update debloat-mint.sh

--- debloat-mint.sh
+++ debloat-mint.sh
@@ -2,7 +2,31 @@
 
 
 ## Remove the bloat
-sudo apt purge redshift libreoffice-core libreoffice-common transmission-gtk hexchat baobab seahorse thunderbird rhythmbox pix simple-scan drawing xed gnote xreader onboard gnome-calendar flatpak 
+sudo apt purge redshift 
+sudo apt purge libreoffice-core
+sudo apt purge libreoffice-common
+sudo apt purge transmission-gtk
+sudo apt purge hexchat
+sudo apt purge baobab
+sudo apt purge seahorse
+sudo apt purge thunderbird
+sudo apt purge rhythmbox
+sudo apt purge pix
+sudo apt purge simple-scan
+sudo apt purge drawing
+sudo apt purge xed
+sudo apt purge gnote
+sudo apt purge xreader
+sudo apt purge onboard
+sudo apt purge gnome-calendar
+sudo apt purge flatpak 
+sudo apt purge gnome-calculator
+sudo apt purge mintstick
+sudo apt purge celluloid
+sudo apt purge timeshift
+sudo apt purge gnome-logs
+sudo apt purge gnome-power-manager
+sudo apt purge onboard
 
 sudo apt autoremove && sudo apt clean
 
