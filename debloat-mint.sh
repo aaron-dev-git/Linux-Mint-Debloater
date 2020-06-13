19713112b4d53471542c9bb683cf07a63c9a585f
arte <ardjanve@gmail.com>
2020-06-13 14:21:48 +0200

    made the script cleaner and faster by doing the purge in one command, fixed typo in readme

--- debloat-mint.sh
+++ debloat-mint.sh
@@ -5,30 +5,29 @@ if [ $? = 0 ]; then
 
 
 	## Remove the bloat
-	sudo apt purge redshift -y
-	sudo apt purge libreoffice-core -y
-	sudo apt purge libreoffice-common -y
-	sudo apt purge transmission-gtk -y
-	sudo apt purge hexchat -y
-	sudo apt purge baobab -y
-	sudo apt purge seahorse -y
-	sudo apt purge thunderbird -y
-	sudo apt purge rhythmbox -y
-	sudo apt purge pix -y
-	sudo apt purge simple-scan -y
-	sudo apt purge drawing -y
-	sudo apt purge xed -y
-	sudo apt purge gnote -y
-	sudo apt purge xreader -y
-	sudo apt purge onboard -y
-	sudo apt purge gnome-calendar -y
-	sudo apt purge gnome-calculator -y
-	sudo apt purge celluloid -y
-	sudo apt purge gnome-logs -y
-	sudo apt purge gnome-power-manager -y
-	sudo apt purge onboard -y
+	sudo apt purge -y redshift \
+		libreoffice-core \
+		libreoffice-commontransmission-gtk \
+		hexchat \
+		baobab \
+		seahorse \
+		thunderbird \
+		rhythmbox \
+		pix \
+		simple-scan \
+		drawing \
+		xed \
+		gnote \
+		xreader \
+		onboard \
+		gnome-calendar \
+		gnome-calculator \
+		celluloid \
+		gnome-logs \
+		gnome-power-manager \
+		onboard
 
-	sudo apt autoremove -y && sudo apt clean
+	sudo apt autoremove -y && sudo apt clean -y
 else
 	exit
 fi
@@ -37,7 +36,7 @@ zenity --question --text "Do you want flatpak and to mark it on hold?" --no-wrap
 if [ $? = 0 ]; then
 
 	##mark packages on hold
-	sudo apt purge flatpak
+	sudo apt purge -y flatpak
 	sudo apt-mark hold flatpak
 else
 	exit
@@ -48,7 +47,7 @@ zenity --question --text "Would you like to update your system?" --no-wrap
 if [ $? = 0 ]; then
 
 	#update and upgrade mint
-	sudo apt update && sudo apt upgrade -y
+	sudo apt update -y && sudo apt upgrade -y
 
 else
 	exit
