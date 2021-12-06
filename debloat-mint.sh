464c4f52e095f38aa67f434d0981d78f2bed1b1c
Aaron-dev <63895016+aaron-dev-git@users.noreply.github.com>
2021-12-06 08:41:05 -0600

    Update for Mint 20.3 and fix typos
    Removed xed from the list of tools to remove, and added Warpinator. Also fixed typo on the section to remove flatpak and mark-hold it.

--- debloat-mint.sh
+++ debloat-mint.sh
@@ -17,23 +17,22 @@ if [ $? = 0 ]; then
 	sudo apt purge pix -y
 	sudo apt purge simple-scan -y
 	sudo apt purge drawing -y
-	sudo apt purge xed -y
 	sudo apt purge gnote -y
 	sudo apt purge xreader -y
 	sudo apt purge onboard -y
 	sudo apt purge gnome-calendar -y
-	sudo apt purge gnome-calculator -y
-	sudo apt purge celluloid -y
+z	sudo apt purge celluloid -y
 	sudo apt purge gnome-logs -y
 	sudo apt purge gnome-power-manager -y
 	sudo apt purge onboard -y
+	sudo apt purge warpinator -y
 
 	sudo apt autoremove -y && sudo apt clean
 else
 	exit
 fi
 
-zenity --question --text "Do you want flatpak and to mark it on hold?" --no-wrap
+zenity --question --text "Do you want remove flatpak and mark it on hold?" --no-wrap
 if [ $? = 0 ]; then
 
 	##mark packages on hold
